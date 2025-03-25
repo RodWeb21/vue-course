78fcd0ac04379c58bcd9f1227b65c3e3acd20984
Rodrigo <rodweb21@gmail.com>
2025-03-25 11:37:30 -0500

    v-if v-else-if v-else

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <script setup>
-let myCondition = true;
+const condition = "";
+const condition2 = false;
 </script>
 
 <template>
-  <!-- <h1 :class="myCondition ? 'hightlight' : 'red'">Hello World</h1> -->
-  <h1 :class="{hightlight: myCondition, red: true}">Hello World</h1>
+  <h1>Directiva v-if</h1>
+  <h2 v-if="condition">La condicion se cumple</h2>
+  <h2 v-else-if="condition2">Condicion 2</h2>
+  <h2 v-else>No se cumplen las condiciones</h2>
 </template>
 
-<style scoped>
-.hightlight {
-  background-color: black;
-  color: yellow;
-}
-
-.red {
-  color: red;
-}
-</style>
+<style scoped></style>
