f8a156c13a1b6a34f5e77f8c55ec8e08d2b1ab9b
Rodrigo <rodweb21@gmail.com>
2025-03-25 08:59:00 -0500

    Estructura y componentes

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HeaderComponent from './components/HeaderComponent.vue';
+
+</script>
+
+<template>
+  <HeaderComponent></HeaderComponent>
+  <h1 class="title">Hello World</h1>
+</template>
+
+<style scoped>
+  .title {
+    background-color: blue;
+    color: white;
+  }
+</style>
