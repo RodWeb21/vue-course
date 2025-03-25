5fe1be77e12503b26f17a6a8285f7e15d27d6ca8
Rodrigo <rodweb21@gmail.com>
2025-03-25 09:37:47 -0500

    Variables y reactividad

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup>
-import HeaderComponent from './components/HeaderComponent.vue';
+import { ref } from 'vue';
 
+  let number = ref(0);
+
+  setInterval(() => {
+    number.value++;
+  }, 1000);
 </script>
 
 <template>
-  <HeaderComponent></HeaderComponent>
-  <h1 class="title">Hello World</h1>
+  <h1>Hello World</h1>
+  <h2>{{ number }}</h2>
 </template>
 
-<style scoped>
-  .title {
-    background-color: blue;
-    color: white;
-  }
-</style>
+<style scoped></style>
