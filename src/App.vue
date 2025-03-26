51222a1866d91feaa81197fd93a2fbbb6d5d28d6
Rodrigo <rodweb21@gmail.com>
2025-03-26 17:22:38 -0500

    props

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,18 @@
 <script setup>
-const movies = ["Lord of the Rings", "Star Wars", "Dune"];
+import ChildComponent from './components/ChildComponent.vue';
 
-const animals = [
-  {
-    id: 1,
-    name: "dog",
-    img: "https://nypost.com/wp-content/uploads/sites/2/2022/12/worlds-cutest-dog-comp-1.jpg",
-  },
-  {
-    id: 2,
-    name: "cat",
-    img: "https://img.freepik.com/foto-gratis/lindo-gatito-domestico-sienta-ventana-mirando-fuera-ia-generativa_188544-12519.jpg",
-  },
-  {
-    id: 3,
-    name: "bird",
-    img: "https://media.cnn.com/api/v1/images/stellar/prod/190414090035-01-cassowary.jpg",
-  },
-];
+const text = 'Info desde variable en el padre';
+const person = {
+  sex: 'hombre',
+  age: 30
+};
 </script>
 
 <template>
-  <h1>Renderizado de listas | v-for</h1>
-  <div>
-    <p v-for="(movie, index) in movies" :key="movie">
-      {{ index + 1 }} - {{ movie }}
-    </p>
-  </div>
+  <h1>Props | Comunicación Padre-hijo</h1>
 
-  <ul>
-    <li v-for="animal in animals" :key="animal.id">
-      {{ animal.name }}
-      <img :src="animal.img" alt="" width="200">
-    </li>
-  </ul>
+  <ChildComponent :msg="text" :person="person"></ChildComponent>
+  <ChildComponent msg="Otro mensaje"></ChildComponent>
 </template>
 
 <style scoped></style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+  defineProps({
+    msg: String,
+    person: {
+      sex: String,
+      age: Number
+    }
+  })
+</script>
+
+<template>
+  <h2>Este es un componente hijo</h2>
+
+  <p>{{ msg }}</p>
+  <div>{{ person?.sex }} - {{ person?.age }}</div>
+</template>
+
+<style></style>
