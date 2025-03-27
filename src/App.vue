12d793d432e5cd4160e2e6249bab98041ef6b07b
Rodrigo <rodweb21@gmail.com>
2025-03-27 09:59:39 -0500

    Formularios

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,42 @@
 <script setup>
-import { ref, watchEffect } from 'vue';
+import { reactive } from 'vue';
 
-const userName = ref("Adrián");
+const movie = reactive({
+  title: "",
+  duration: 0,
+  director: ""
+});
 
-const setName = () => {
-  userName.value = "María";
-};
-
-watchEffect(
-  () => {
-    console.log("User Name modified to: ", userName.value);
+const handleSubmit = () => {
+  if (!movie.title || !movie.duration || !movie.director) {
+    alert("Rellena todos los campos, por favor");
+    return;
   }
-);
+  console.log(movie);
+};
 </script>
 
 <template>
-  <h1>Watch | Vigilancia de cambios</h1>
+  <h1>Forms</h1>
 
-  <h2>Hola {{ userName }}</h2>
+  <form @submit.prevent="handleSubmit" class="form">
+    <label for="title">Title</label>
+    <input v-model="movie.title" type="text" id="title">
 
-  <button @click="setName">Cambiar de usuario</button>
+    <label for="duration">Duration</label>
+    <input v-model="movie.duration" type="number" id="duration">
+
+    <label for="director">Director</label>
+    <input v-model="movie.director" type="text" id="director">
+
+    <button>Enviar</button>
+  </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
