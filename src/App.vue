bc92c1678c89de8e31e9041ffccc4633c0cdeaec
Rodrigo <rodweb21@gmail.com>
2025-03-27 15:55:05 -0500

    vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,10 @@
 <script setup>
-import { reactive } from 'vue';
+import { RouterView } from 'vue-router';
 
-const movie = reactive({
-  title: "",
-  duration: 0,
-  director: ""
-});
-
-const handleSubmit = () => {
-  if (!movie.title || !movie.duration || !movie.director) {
-    alert("Rellena todos los campos, por favor");
-    return;
-  }
-  console.log(movie);
-};
 </script>
 
 <template>
-  <h1>Forms</h1>
-
-  <form @submit.prevent="handleSubmit" class="form">
-    <label for="title">Title</label>
-    <input v-model="movie.title" type="text" id="title">
-
-    <label for="duration">Duration</label>
-    <input v-model="movie.duration" type="number" id="duration">
-
-    <label for="director">Director</label>
-    <input v-model="movie.director" type="text" id="director">
-
-    <button>Enviar</button>
-  </form>
+  <RouterView />
 </template>
 
-<style scoped>
-.form {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-}
-</style>
+<style scoped></style>
